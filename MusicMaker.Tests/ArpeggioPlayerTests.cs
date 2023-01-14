6ca0949baaaa07ba11f5afd50de99454fc992c56
michael_rosario <michael.rosario@designinteractive.net>
2023-01-14 08:36:36 -0500

    feat ( arp player ) execute many players

--- MusicMaker.Tests/ArpeggioPlayerTests.cs
+++ MusicMaker.Tests/ArpeggioPlayerTests.cs
@@ -20,10 +20,8 @@ namespace MusicMaker.Tests
             var channel = 1;
 
             var track = new ChordPlayerTrack(instrument, channel, tempo);
-            
-            var command = MakeArpeggioPatternCommand1();
 
-            var player = new ArpeggioPlayer(track,command);
+            var player = new ArpeggioPlayer(track,MakeArpeggioPatternCommand1());
             var chordChanges = new List<ChordChange>
             {
                 new(Note.Parse("A4").NoteNumber, ChordType.Minor, 4),
@@ -38,6 +36,87 @@ namespace MusicMaker.Tests
             midiFile.Chunks.Add(track.MakeTrackChunk());
             midiFile.Write("arp1.mid", true);
         }
+        
+        [Test]
+        public void ArpeggioPlayer__Arp2Test()
+        {
+            var tempo = 180;
+            var instrument = Instruments.AcousticGrandPiano;
+            var channel = 1;
+
+            var track = new ChordPlayerTrack(instrument, channel, tempo);
+            var track2 = new ChordPlayerTrack(instrument, channel, tempo);
+            var track3 = new ChordPlayerTrack(instrument, channel, tempo);
+
+            var player = new ArpeggioPlayer(track,MakeArpeggioPatternCommand2());
+            var player2 = new ArpeggioPlayer(track2,MakeArpeggioPatternCommand1());
+            var player3 = new ArpeggioPlayer(track3,MakeArpeggioPatternCommand3());
+            
+            var chordChanges = YourGraceIsEnoughChords();
+
+            player.PlayFromChordChanges(chordChanges);
+            player2.PlayFromChordChanges(chordChanges);
+            player3.PlayFromChordChanges(chordChanges);
+            
+            var midiFile = new MidiFile();
+            //midiFile.Chunks.Add(track.MakeTrackChunk());
+            midiFile.Chunks.Add(track2.MakeTrackChunk());
+            midiFile.Chunks.Add(track3.MakeTrackChunk());
+            midiFile.Write("3.mid", true);
+        }
+
+        private static List<ChordChange>? YourGraceIsEnoughChords()
+        {
+            var chordChanges = new List<ChordChange>
+            {
+                new(Note.Parse("G4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("G4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("C4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("C4").NoteNumber, ChordType.Major, 4),
+
+                new(Note.Parse("G4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("G4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("C4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("C4").NoteNumber, ChordType.Major, 4),
+
+                new(Note.Parse("E4").NoteNumber, ChordType.Minor, 4),
+                new(Note.Parse("D4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("C4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("C4").NoteNumber, ChordType.Major, 4),
+
+                new(Note.Parse("G4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("G4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("C4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("C4").NoteNumber, ChordType.Major, 4),
+
+                new(Note.Parse("E4").NoteNumber, ChordType.Minor, 4),
+                new(Note.Parse("D4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("C4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("C4").NoteNumber, ChordType.Major, 4),
+
+                new(Note.Parse("A4").NoteNumber, ChordType.Minor, 4),
+                new(Note.Parse("G4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("C4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("D4").NoteNumber, ChordType.Major, 4),
+
+                new(Note.Parse("E4").NoteNumber, ChordType.Minor, 4),
+                new(Note.Parse("D4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("C4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("C4").NoteNumber, ChordType.Major, 4),
+
+
+                new(Note.Parse("G4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("D4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("E4").NoteNumber, ChordType.Minor, 4),
+                new(Note.Parse("C4").NoteNumber, ChordType.Major, 4),
+
+                new(Note.Parse("G4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("D4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("C4").NoteNumber, ChordType.Major, 4),
+                new(Note.Parse("C4").NoteNumber, ChordType.Major, 4),
+            };
+            return chordChanges;
+        }
 
         private MakeArpeggioPatternCommand MakeArpeggioPatternCommand1()
         {
@@ -47,12 +126,12 @@ namespace MusicMaker.Tests
                 {
                     Rows = new()
                     {
-                        new() { Type = ArpeggioPatternRowType.Fifth, Octave = 2, Pattern = "----|----|--s-|----|" },
-                        new() { Type = ArpeggioPatternRowType.Third, Octave = 2, Pattern = "----|----|s---|s---|" },
-                        new() { Type = ArpeggioPatternRowType.Root, Octave = 2, Pattern =  "----|--s-|----|--s-|" },
-                        new() { Type = ArpeggioPatternRowType.Fifth, Octave = 1, Pattern = "----|s---|----|----|" },
-                        new() { Type = ArpeggioPatternRowType.Third, Octave = 1, Pattern = "--s-|----|----|----|" },
-                        new() { Type = ArpeggioPatternRowType.Root, Octave = 1, Pattern =  "s---|----|----|----|" },
+                        new() { Type = ArpeggioPatternRowType.Fifth, Octave = 2, Pattern = "----|----|----|---s  |" },
+                        new() { Type = ArpeggioPatternRowType.Third, Octave = 2, Pattern = "----|--s-|s---|s---|" },
+                        new() { Type = ArpeggioPatternRowType.Root, Octave = 2, Pattern =  "---s|-s-s|---s|-s--|" },
+                        new() { Type = ArpeggioPatternRowType.Fifth, Octave = 1, Pattern = "--s-|s---|--s-|--s-|" },
+                        new() { Type = ArpeggioPatternRowType.Third, Octave = 1, Pattern = "-s--|----|-s--|----|" },
+                        new() { Type = ArpeggioPatternRowType.Root, Octave = 1, Pattern =  "s---|----|s---|----|" },
                     },
                     InstrumentNumber = Instruments.Banjo
                 },
@@ -62,5 +141,54 @@ namespace MusicMaker.Tests
             };
             return command;
         }
+        
+        private MakeArpeggioPatternCommand MakeArpeggioPatternCommand2()
+        {
+            var command = new MakeArpeggioPatternCommand
+            {
+                Pattern = new ArpeggioPattern()
+                {
+                    Rows = new()
+                    {
+                        new() { Type = ArpeggioPatternRowType.Fifth, Octave = 2, Pattern = "----|----|----|----|" },
+                        new() { Type = ArpeggioPatternRowType.Third, Octave = 2, Pattern = "----|----|----|----|" },
+                        new() { Type = ArpeggioPatternRowType.Root, Octave = 2, Pattern =  "----|----|----|----|" },
+                        new() { Type = ArpeggioPatternRowType.Fifth, Octave = 1, Pattern = "--s-|--s-|--s-|s--s|" },
+                        new() { Type = ArpeggioPatternRowType.Third, Octave = 1, Pattern = "--s-|--s-|--s-|s--s|" },
+                        new() { Type = ArpeggioPatternRowType.Root, Octave = 1, Pattern =  "--s-|--s-|--s-|s--s|" },
+                    },
+                    InstrumentNumber = Instruments.Banjo
+                },
+                UserId = "mrosario",
+                BeatsPerMinute = 120,
+                Channel = 0
+            };
+            return command;
+        }
+        
+        private MakeArpeggioPatternCommand MakeArpeggioPatternCommand3()
+        {
+            var command = new MakeArpeggioPatternCommand
+            {
+                Pattern = new ArpeggioPattern()
+                {
+                    Rows = new()
+                    {
+                        new() { Type = ArpeggioPatternRowType.Fifth, Octave = 2, Pattern = "----|----|----|----|" },
+                        new() { Type = ArpeggioPatternRowType.Third, Octave = 2, Pattern = "----|----|----|----|" },
+                        new() { Type = ArpeggioPatternRowType.Root, Octave = 2, Pattern =  "----|----|----|----|" },
+                        new() { Type = ArpeggioPatternRowType.Fifth, Octave = 1, Pattern = "----|----|----|---s|" },
+                        new() { Type = ArpeggioPatternRowType.Third, Octave = 1, Pattern = "----|----|----|----|" },
+                        new() { Type = ArpeggioPatternRowType.Root, Octave = 1, Pattern =  "s-s-|s-s-|s-s-|s-s-|" },
+                    },
+                    InstrumentNumber = Instruments.Banjo
+                },
+                UserId = "mrosario",
+                BeatsPerMinute = 120,
+                Channel = 0
+            };
+            return command;
+        }        
+        
     }
 }
