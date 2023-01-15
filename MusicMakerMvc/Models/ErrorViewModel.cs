8f7d03bd9e0241af0ada7225aaadece65702a59f
michael_rosario <michael.rosario@designinteractive.net>
2023-01-15 13:45:12 -0500

    feat ( mvc ) add mvc project

---
+++ MusicMakerMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MusicMakerMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
