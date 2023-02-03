06775cddc32254ac32966d8d56901914ddaeefe9
michael_rosario <michael.rosario@designinteractive.net>
2023-02-02 19:36:40 -0500

    feat ( firebase ) deploy and small fixes

--- ng-music-maker/src/app/core/services/server-helper.ts
+++ ng-music-maker/src/app/core/services/server-helper.ts
@@ -4,11 +4,6 @@ export class ServerHelper
 {
   static getApiUrl()
   {
-    if(!environment.production){
-      return environment.apiUrl;
-    } else {      
-      let parts = window.location.href.split('/');
-      return parts[0] + "//" + parts[2];
-    }
+    return environment.apiUrl;
   }
 }
--- ng-music-maker/src/environments/environment.ts
+++ ng-music-maker/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl:"https://music-maker.v6.rocks"
+  apiUrl:"http://localhost:5000"
 };
 
 /*
