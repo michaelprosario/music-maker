82e1632401d2617d0281830c87817c96b0c08030
michael_rosario <michael.rosario@designinteractive.net>
2023-02-04 13:24:27 -0500

    feat ( env file ) small comment

--- ng-music-maker/src/environments/environment.ts
+++ ng-music-maker/src/environments/environment.ts
@@ -2,9 +2,10 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// apiUrl:"http://localhost:5000",
 export const environment = {
   production: false,
-  apiUrl:"http://localhost:5000",
+  apiUrl:"https://music-maker.v6.rocks",
   midiBlobStorage: "https://rosarioappdata.blob.core.windows.net/midi-files"
 };
 
