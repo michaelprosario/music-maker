96182e17fd973be81177bec7e9c42e5f3f5097a0
michaelprosario <michael.p.rosario@gmail.com>
2023-03-05 12:42:28 +0000

    feat ( midi playground ) first commit

---
+++ MidiPlayground/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Melanchall.DryWetMidi.Common;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Interaction;
+using Melanchall.DryWetMidi.MusicTheory;
+
+namespace MidiPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var midiFile = MidiFile.Read("seek-ye-first.mid");
+
+            Console.WriteLine("Chords:");
+
+            foreach (var chord in midiFile.GetChords())
+            {
+                if(chord.Notes.Count() > 1){
+                    Console.Write($@"
+                    chord
+                    channel = {chord.Channel}
+                    time = {chord.Time}
+                    length = {chord.Length}
+                    notes:");
+
+                    Console.WriteLine(chord.ToString());
+                }
+            }
+        }
+    }
+}
