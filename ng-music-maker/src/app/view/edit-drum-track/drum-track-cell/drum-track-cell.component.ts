5ed8724ebca63322f87e5c52b06a91cf3d3762dc
michael_rosario <michael.rosario@designinteractive.net>
2023-03-18 07:39:34 -0400

    feat ( drummer ) refine colors

--- ng-music-maker/src/app/view/edit-drum-track/drum-track-cell/drum-track-cell.component.ts
+++ ng-music-maker/src/app/view/edit-drum-track/drum-track-cell/drum-track-cell.component.ts
@@ -7,11 +7,33 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class DrumTrackCellComponent implements OnInit {
 
-  @Input()
-  trackCellValue: number = 0;
+  @Input() trackCellValue: number = 0;
+  @Input() cellIndex: number = 0;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  setCellStyle(){
+
+    let backgroundColor = '';
+    if(this.trackCellValue === 0)
+    {
+      if(this.cellIndex % 4 === 0)
+      {
+        backgroundColor = '#C09BD8';
+      }
+      else
+      {
+        backgroundColor = '#EDE3E9';
+      }
+    }
+    else
+    {
+      backgroundColor = 'purple';
+    }
+    
+    return {'background-color': backgroundColor}
+  }  
 
 }
